ff18117ce7fff10c0df6a76dce2246445d082679
Lumeriino <merinomansoluciaadobe@gmail.com>
2024-10-25 18:58:20 +0200

    FAQ

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// FAQ
+const icons = document.querySelectorAll('.fa-plus');
+
+icons.forEach(icon => {
+  icon.addEventListener('click', () => {
+
+    const respuesta = icon.parentElement.nextElementSibling;
+
+    icon.classList.toggle('icon');
+
+    respuesta.style.display = respuesta.style.display === 'none' || respuesta.style.display === '' ? 'block' : 'none';
+  });
+});
