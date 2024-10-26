b5ae0b21d79ac15fd1a6dbc2c5757c58a8b1ba51
Lumeriino <merinomansoluciaadobe@gmail.com>
2024-10-26 11:23:19 +0200

    menu_sinfuncionar

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-function changeOpacity() {
-  var scroll = window.scrollY;
-
-  var menu = document.getElementById("menu").clientHeight;
-
-  var max_scroll = window.innerHeight - menu;
-
-  if (scroll <= max_scroll) {
-    var opacity = scroll / max_scroll;
-
-    var menuElem = document.getElementById("menu");
-    menuElem.style.backgroundColor = "rgb(0, 0, 210, " + opacity +")";
-
-    menuElem.style.color = "white";
-  }
-}
-
-window.onscroll = function () {
-  changeOpacity() ;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-
-  function goBlack() {
-    var menu = document.getElementById("menu");
-    menu.style.backgroundColor = "black";
-  }
-
-  var links = document.querySelectorAll("#menu a[href^=`#´]");
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", goBlack);
-  }
-} );
 
 // FAQ
 const icons = document.querySelectorAll('.fa-plus');
