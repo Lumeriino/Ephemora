2eea42e473325c83aecaca28c09b5608b9b8d84c
Lumeriino <merinomansoluciaadobe@gmail.com>
2024-10-26 17:56:14 +0200

    menu_funciona

--- js/script.js
+++ js/script.js
@@ -20,3 +20,29 @@ icons.forEach(icon => {
   });
 });
 
+// Selecciona el menú y el icono del menú
+const menu = document.getElementById("menu");
+const menuToggle = document.getElementById("menuToggle");
+const closeMenu = document.getElementById("closeMenu");
+const menuLinks = document.querySelectorAll(".menuLink");
+
+// Función para alternar el menú
+function toggleMenu() {
+    menu.classList.toggle("active"); // Alternar la clase 'active' en el menú
+}
+
+// Función para cerrar el menú
+function closeMenuHandler() {
+    menu.classList.remove("active"); // Quitar la clase 'active' para cerrar el menú
+}
+
+// Asignar el evento de clic al ícono del menú
+menuToggle.addEventListener("click", toggleMenu);
+
+// Asignar el evento de clic al ícono de cerrar menú
+closeMenu.addEventListener("click", closeMenuHandler);
+
+// Asignar evento a los enlaces del menú
+menuLinks.forEach(link => {
+    link.addEventListener("click", closeMenuHandler);
+});
