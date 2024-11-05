473fa09264964e33609b72b4f6eb30e2042ececb
agusestevezr <agus.estevez.r@gmail.com>
2024-11-05 14:46:58 +0100

    cart.html

--- js/script.js
+++ js/script.js
@@ -208,3 +208,20 @@ var typed = new Typed('#number', {
   loop: true,
 });
 
+//CART
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
